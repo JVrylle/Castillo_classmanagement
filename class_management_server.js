b9a492b0d8a11d9c3e092b4772de6ab9d75b60ae
Vrylle <163834092+JVrylle@users.noreply.github.com>
2025-08-14 09:33:04 +0800

    Add files via upload

---
+++ class_management_server.js
@@ -0,0 +1,57 @@
+const express = require(`express`);
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+let users = [
+    {id: 1, lastName:`castillo`,firstName:`josh vrylle`,section:`bsit 4a`,status:`p`},
+    {id: 2, lastName:`hernandez`,firstName:`marc lester`,section:`bsit 4a`,status:`p`},
+]
+
+
+app.get(`/`,(req,res) => {
+    const {lastName, firstName, section, status } = req.body;
+
+    const userIndex = users.findIndex(user => user.firstName === firstName && user.lastName === lastName);
+
+    if (userIndex !== -1) {
+         
+        users[userIndex].status = status;
+        console.log(`Updated attendance for ${lastName} ${firstName} ${section} to: ${status}`);
+        res.status(200).json({ message: `Attendance for ${lastName} ${firstName} has been update to ${status}`})
+
+    } else {
+        const newUser = {
+            id: users.length +1,
+            firstName,
+            lastName,
+            section,
+            status,
+        };
+
+        users.push(newUser);
+            console.log(`New user added: ${lastName} ${firstName} with status: ${status}`);
+
+
+            res.status(201).json({ message: `New student ${lastName} ${firstName} hast been added with status:${status}.`});
+
+    };
+});
+
+
+app.get('/users', (req,res) => {
+    res.status(200).json(users);
+});
+
+app.get('/', (req,res) => {
+    res.send('Server is up and running!');
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Success at http://localhost:${PORT}`)
+});
+
+
+module.exports = app;
